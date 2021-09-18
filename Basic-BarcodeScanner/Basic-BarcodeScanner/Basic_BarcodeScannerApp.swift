753dde6ebb06d6b17c1a98aa39bb07fdeb440b02
Raul Gutierrez Niubo <raulgniubo@gmail.com>
2021-09-18 11:55:10 -0400

    Created xcode project

---
+++ Basic-BarcodeScanner/Basic-BarcodeScanner/Basic_BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_BarcodeScannerApp.swift
+//  Basic-BarcodeScanner
+//
+//  Created by Raul Gutierrez Niubo on 9/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Basic_BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
